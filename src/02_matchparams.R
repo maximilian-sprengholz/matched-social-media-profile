3a75946538384a02e12982431ca042cf1e96f3c0
sprenmax <maximilian.sprengholz@hu-berlin.de>
2022-08-15 17:43:03 +0200

    Changed family icon to free version

--- src/02_matchparams.R
+++ src/02_matchparams.R
@@ -200,7 +200,7 @@ matchparams <- dict(list(
 
     # Family
     family = dict(list(
-        hl = list(label="Familie", icon="fa-people-group"),
+        hl = list(label="Familie", icon="fa-users"),
         items = dict(list(
             marital = dict(list(
                 weight = function(value=NA, common=0) {
